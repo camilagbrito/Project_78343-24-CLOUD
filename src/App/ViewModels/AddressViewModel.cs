15328c22e62d5021d203973355248a087018f498
Camila Brito <camila.g.brito48@gmail.com>
2024-04-26 22:20:03 +0100

    Login and Register

---
+++ src/App/ViewModels/AddressViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace App.ViewModels
+{
+    public class AddressViewModel
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [DisplayName("Rua")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DisplayName("Número")]
+        [StringLength(5, ErrorMessage = "Máximo 5 caracteres")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [DisplayName("Concelho")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [DisplayName("Distrito")]
+        public string Region { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(8, ErrorMessage = "Máximo 8 caracteres")]
+        [DisplayName("Código Postal")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [DisplayName("País")]
+        public string Country { get; set; }
+        public string UserId {  get; set; }
+    }
+}
---
+++ src/App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Business.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace App.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Informe o email")]
+        [Display(Name = "Email")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password, ErrorMessage ="Senha deve conter letras, número e caracteres especiais")]
+        [MinLength(6, ErrorMessage ="Mínimo 6 caracteres")]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DisplayName("Nome")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DisplayName("Apelido")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DisplayName("Data de Nascimento")]
+        [DataType(DataType.DateTime)]
+        public DateTime BirthDate { get; set; }
+    }
+}
